92d364f4148b9dd48d5e646d583c9798fd02c535
perdub <aleksejspakovskij968@gmail.com>
2023-01-05 15:17:30 +0300

    Edit GameInfo

--- Libarys/Types/Miku.Types.Base/GameInfo.cs
+++ Libarys/Types/Miku.Types.Base/GameInfo.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Miku.Types.Base
 {
-    class GameInfo
+    /// <summary>
+    /// Provide information about type, who can be attached to <c>Miku</c>
+    /// </summary>
+    public class GameInfo
     {
+        /// <summary>
+        /// Type version
+        /// </summary>
+        public Version Version { get; set; }
+        /// <summary>
+        /// Name to render, it`s need only to modding. You don`t need to change this if type is main
+        /// </summary>
+        public string RenderName { get; set; } = "game";
     }
 }
